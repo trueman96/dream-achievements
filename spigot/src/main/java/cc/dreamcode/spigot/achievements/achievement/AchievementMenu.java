149c9253bc87cdade38797fadb48e9eb4a080c50
trueman96 <sergiusz22@protonmail.com>
2024-01-01 17:09:52 +0100

    made some code improvements

--- spigot/src/main/java/cc/dreamcode/spigot/achievements/achievement/AchievementMenu.java
+++ spigot/src/main/java/cc/dreamcode/spigot/achievements/achievement/AchievementMenu.java
@@ -5,6 +5,7 @@ import cc.dreamcode.menu.bukkit.BukkitMenuProvider;
 import cc.dreamcode.menu.bukkit.base.BukkitMenu;
 import cc.dreamcode.spigot.achievements.config.PluginConfig;
 import eu.okaeri.injector.annotation.Inject;
+import eu.okaeri.tasker.core.Tasker;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -15,28 +16,34 @@ import java.util.Map;
 @RequiredArgsConstructor(onConstructor_ = @Inject)
 public class AchievementMenu {
 
+    private final Tasker tasker;
     private final PluginConfig pluginConfig;
     private final BukkitMenuProvider bukkitMenuProvider;
 
     public void open(Player player) {
         BukkitMenuBuilder menuBuilder = this.pluginConfig.achievementMenuBuilder;
-        BukkitMenu menu = menuBuilder.buildEmpty();
-        for (Map.Entry<Integer, ItemStack> entry : menuBuilder.getItems().entrySet()) {
-            int slot = entry.getKey();
-            ItemStack item = entry.getValue();
-            if (item == null || item.getType().equals(Material.AIR)) {
-                continue;
-            }
+        this.tasker.newChain()
+                .supplyAsync(() -> this.pluginConfig.achievementMenuBuilder.buildEmpty())
+                .acceptAsync(menu -> {
+                    for (Map.Entry<Integer, ItemStack> entry : menuBuilder.getItems().entrySet()) {
+                        int slot = entry.getKey();
+                        ItemStack item = entry.getValue();
+                        if (item == null || item.getType().equals(Material.AIR)) {
+                            continue;
+                        }
 
-            AchievementType achievementType = this.pluginConfig.achievementTypeSlot.get(slot);
-            if (achievementType == null) {
-                menu.setItem(slot, item);
-                continue;
-            }
+                        AchievementType achievementType = this.pluginConfig.achievementTypeSlot.get(slot);
+                        if (achievementType == null) {
+                            menu.setItem(slot, item);
+                            continue;
+                        }
 
-            menu.setItem(slot, item, event -> {
+                        menu.setItem(slot, item, event -> {
 
-            });
-        }
+                        });
+                    }
+                })
+                .acceptSync(menu -> menu.open(player))
+                .execute();
     }
 }
